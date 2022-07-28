f2f3324f7c435d06018caed41d2e38a6b2dc05da
mwilson <maegan@maeganwilson.com>
2022-07-28 16:26:22 -0500

    build: danger danger

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,2 +1,14 @@
 import Foundation
 import Danger
+
+let danger = Danger()
+let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
+
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
+let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+
+if !changelogChanged && sourceChanges != nil {
+  warn("No CHANGELOG entry added.")
+} else {
+    message("✅ Changelog remembered")
+}
